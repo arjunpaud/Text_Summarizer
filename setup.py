9298a70f6027e5e0a334c549b7944b3d58463b3d
Arjun Paudel <arjupau@gmail.com>
2024-06-24 18:11:08 +0545

    error correction

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+    long_description_content = f.read()
 
 
 __version__ = "0.0.0"
@@ -19,9 +19,9 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="Text Summarizer Package",
-    long_description=long_description,
+    long_description=long_description_content,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/arjunpaud/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/arjunpaud/{REPO_NAME}/issues",
     },
